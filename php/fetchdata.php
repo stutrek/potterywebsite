265857daaebf06651fdffcfd6aa1cde2f0ea5895
Stu Kabakoff <sakabako@gmail.com>
2012-04-19 00:53:57 -0400

    added more test data and added margin to the product list.

--- php/fetchdata.php
+++ php/fetchdata.php
@@ -1,5 +1,6 @@
 <?php
 header('content-type: application/json');
 ?>[
-	{"title": "Bowl", "imageName": "1-bowl.jpg", "id":1, "price": 50.00, "description":"This is a beautiful bowl big enough for soup or a LOT of cereal."}
+	{"title": "Bowl", "imageName": "1-bowl.jpg", "id":1, "price": 50.00, "description":"This is a beautiful bowl big enough for soup or a LOT of cereal."},
+	{"title": "Bowls", "imageName": "2-bowls.jpg", "id":2, "price": 30.00, "description":"These bowls are great for rice or small dishes."}
 ]
