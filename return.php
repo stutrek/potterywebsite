59185437b789f4ae2fa2154d8885b2cad7d0e9e0
Stu Kabakoff <sakabako@gmail.com>
2012-04-26 00:13:05 -0400

    fixed return.php

--- return.php
+++ return.php
@@ -2,10 +2,6 @@
 require("php/db_interface.php");
 header('content-type: text/plain');
 
-$product = get_product($_GET['id']);
-
+$product = get_product($_GET['product']);
 $product['available'] = 0;
-
-print_r($product);
-
 update_product($product);
